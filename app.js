a3e2b05b41b77a8baaf0d3856fde764b364b7408
LUCA FORMA <117093377+lucaf1990@users.noreply.github.com>
2023-07-03 16:24:40 +0200

    categories

--- app.js
+++ app.js
@@ -24,17 +24,63 @@ const fetchQuestion = async (category, limit) => {
     console.log("Error with the request");
   }
 };
-fetchQuestion();
+
+const allCategoryArray = [
+  "Literature",
+  "Language",
+  "Science",
+  "General",
+  "People Places",
+  "Geography",
+  "History",
+  "Entertainment",
+  "Toys Game",
+  "Music",
+  "Mathematics",
+  "Food and Drinks",
+  "Mythology",
+  "Sports",
+];
+
+const numberOfQuestions = [5, 10, 15, 30];
 
 const playButton = document.querySelector(".button");
 const welcomeDiv = document.querySelector("#welcome");
-const category = document.querySelector(".pickCategory");
-category.className = "hide";
+const pickCategory = document.querySelector(".pickCategory");
+const category = document.querySelector(".category");
+const allCategory = document.querySelector("#allCategory");
+pickCategory.className = "hide";
+
 const buttonPlay = () => {
   playButton.addEventListener("click", () => {
     welcomeDiv.className = "hide";
     playButton.className = "hide";
-    category.className = "visible";
+    pickCategory.className = "visible";
+    allCategoryArray.forEach((cat) => {
+      const div = document.createElement("div");
+      div.textContent = cat;
+
+      category.appendChild(div);
+      div.className = "category";
+    });
+
+    allCategory.appendChild(category);
   });
 };
+
 buttonPlay();
+allCategory.addEventListener("mousemove", (event) => {
+  const scrollSpeed = 5;
+  const direction = event.movementX > 0 ? 1 : -1;
+  allCategory.scrollLeft += direction * scrollSpeed;
+});
+
+let chooseCategory = "";
+const chooseMyCategory = () => {
+  category.addEventListener("click", (event) => {
+    chooseCategory = event.target.textContent.toLowerCase().split(" ").join("");
+    console.log(chooseCategory);
+  });
+};
+
+document.addEventListener("DOMContentLoaded", chooseMyCategory);
