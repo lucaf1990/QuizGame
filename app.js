b423b2f37036ee97be8848fad849991d1994fbec
LUCA FORMA <117093377+lucaf1990@users.noreply.github.com>
2023-07-05 15:07:59 +0200

    riddle done

--- app.js
+++ app.js
@@ -53,14 +53,13 @@ let value = "";
 riddle.className = "hide";
 
 pickCategory.className = "hide";
-
+const body = document.querySelector("body");
 const buttonPlay = () => {
   playButton.addEventListener("click", () => {
     welcomeDiv.className = "hide";
     riddle.className = "hide";
     playButton.className = "hide";
     pickCategory.className = "visible";
-    pickCategory.style = "cursor:pointer";
   });
   choice.addEventListener("click", async () => {
     pickCategory.className = "hide";
@@ -69,13 +68,17 @@ const buttonPlay = () => {
     let p = document.createElement("p");
     let container = document.createElement("div");
     let input = document.createElement("input");
+    input.placeholder = "";
+    input.setAttribute("autofocus", "true");
     let button = document.createElement("button");
     input.className = "inputRiddle";
     let question = await fetchIndovinello();
+    body.style = "animation:none";
     p.innerText = question.question;
     title.innerText =
-      "YOU MUST UNLOCK THE SECRET OF THE RIDDLE TO REVEAL THE HIDDEN PATH";
-    button.innerHTML = "CHECK";
+      "YOU MUST UNLOCK THE SECRET OF THE EYE \n TO REVEAL THE HIDDEN PATH";
+
+    button.innerHTML = "TRY";
     riddle.appendChild(title);
     riddle.appendChild(p);
     riddle.append(container);
@@ -84,10 +87,12 @@ const buttonPlay = () => {
     container.appendChild(button);
 
     const riddleInput = document.querySelector(".inputRiddle");
-    const message = document.createElement("p");
+    const message = document.createElement("span");
     message.className = "message";
     riddle.appendChild(message);
-
+    setTimeout(() => {
+      message.innerText = "If you never try you will never know";
+    }, 1000);
     riddleInput.addEventListener("change", (e) => {
       value = e.target.value;
     });
@@ -107,16 +112,19 @@ const buttonPlay = () => {
           div.className = "category";
         });
         riddle.style = "display:none";
+        pickCategory.className = "hide";
         const p = document.createElement("p");
         p.innerText = "Generate ranodm number";
-
         allCategory.appendChild(category);
         pickCategory.appendChild(p);
+      } else if (value === "") {
+        message.innerText = "";
       } else {
-        message.innerText = "Wrong answer. Try again.";
+        message.innerText = "Wrong";
         setTimeout(() => {
           message.innerText = "";
-        }, 1000);
+        }, 2000);
+
         const div = document.createElement("div");
         riddle.appendChild(div);
         let timer = setTimeout(() => {
@@ -125,7 +133,7 @@ const buttonPlay = () => {
             div.innerText = question.answer;
           });
           clearTimeout(timer);
-        }, 10000);
+        }, 0);
       }
     });
   });
